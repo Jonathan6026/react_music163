3a66b66b88c2d60a3e1590be99180276a4f5a771
Li <Jonathan.Li@osram.com>
2021-05-16 15:15:00 +0800

    增加头部菜单styled和router

--- src/components/app-header/index.js
+++ src/components/app-header/index.js
@@ -1,12 +1,60 @@
 import React,{memo} from 'react'
+
+//从localdata导入路由映射headerLinks
+import {headerLinks} from "@/common/local-data"
+
+import {
+    HeaderWrapper,
+    HeaderLeft,
+    HeaderRight
+} from "./style"
+
 import { NavLink } from 'react-router-dom'
 
 export default memo(function JLAppHeader(){
+    //页面函数(1)展示顶部list
+    const showSelectItem = (item,index) => {
+        if (index < 3) {
+            return(
+                <NavLink exact to = {item.link} >
+                    {item.title}
+                    <i className="sprite_01 icon"></i>
+                </NavLink>
+            )
+        }else{
+            return(
+                <a href={item.link} >
+                    {item.title}
+                </a>
+            )
+        }
+    }
+
+
+    //返回JSX
     return(
-        <div>
-            <NavLink to="/">发现</NavLink>
+        <HeaderWrapper>
+            <div className="content w1100">
+                <HeaderLeft>
+                    <a href="#/" className= "logo sprite_01"></a>
+                    <div className="header-group">
+                        {headerLinks.map((item,index)=>{
+                            return(
+                                <div key = {item.title} className="header-item">
+                                    {showSelectItem(item,index)}
+                                </div>
+                            )
+                        })}
+                    </div>
+                </HeaderLeft>
+                <HeaderRight>右</HeaderRight>
+            </div>
+            <div className="divider">
+
+            </div>
+            {/* <NavLink to="/">发现</NavLink>
             <NavLink to="/mine">我的音乐</NavLink>
-            <NavLink to="/friend">好友</NavLink>
-        </div>
+            <NavLink to="/friend">好友</NavLink> */}
+        </HeaderWrapper>
     )
 })
---
+++ src/components/app-header/style.js
@@ -0,0 +1,89 @@
+import styled from "styled-components"
+
+export const HeaderWrapper = styled.div`
+    height: 75px;
+
+    background-color: #242424;
+    
+    .content {
+        height: 70px;
+ 
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .divider {
+        height: 5px;
+        background-color: #C20C0C;
+    }
+`
+//Header左
+export const HeaderLeft = styled.div`
+  display: flex;
+
+  .logo {
+    display: inline-block;
+    width: 176px;
+    height: 69px;
+    background-position: 0 0;
+    text-indent: -9999px;
+  }
+
+  .header-group {
+    display: flex;
+    font-size: 14px;
+    
+    .header-item {
+      position: relative;
+      padding: 0 19px;
+      height: 70px;
+      text-align: center;
+      line-height: 70px;
+
+      a {
+        color: white;
+      }
+
+      &:hover { 
+        text-decoration: none;
+        background-color: #000;
+      }
+
+      /* hot图标 */
+      :last-of-type a {
+        position: relative;
+        ::after {
+          position: absolute;
+          content: '';
+          width: 28px;
+          height: 19px;
+          background-image: url(${require('@/assets/img/sprite_01.png').default});
+          background-position: -190px 0;
+          top: -15px;
+          right: -25px;
+        }
+      }
+
+
+      .active .icon  {
+        position: absolute;
+        display: inline-block;
+        width: 12px;
+        height: 7px;
+        bottom: -1px;
+        left: 50%;
+        transform: translate(-50%, 0);
+        background-image: url('${require('@/assets/img/sprite_01.png').defaultA}');
+        background-position: -226px 0;
+      }
+    }
+
+  }
+
+  
+`
+
+//Header右
+export const HeaderRight = styled.div`
+
+`
