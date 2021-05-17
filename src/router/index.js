33730b27a28bee0d60c21957290a62bf56f33328
Li <Jonathan.Li@osram.com>
2021-05-17 19:19:30 +0800

    头路由优化

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,21 @@
+import React from 'react'
+
 import JLDiscover from "@/pages/discover"
 import JLFriend from "@/pages/friend"
 import JLMine from "@/pages/mine"
+import { Redirect } from "react-router"
 
 const routes = [
     {
         path:"/",
         // 不加有可能后续路由辨识错误
         exact:true,   
-        component:JLDiscover
+        // component:JLDiscover
+        render:() => {
+            return(
+                <Redirect to="/discover"></Redirect>   //重定向
+            )
+        }
     },
     {
         path:"/mine",
